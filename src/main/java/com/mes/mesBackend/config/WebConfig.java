89a1061e96ebac0629f55d2d3332d1b6725ae7f9
heeeun <gmldms7868@naver.com>
2021-10-05 16:39:52 +0900

    cors 설정

---
+++ src/main/java/com/mes/mesBackend/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.mes.mesBackend.config;
+
+import io.swagger.models.HttpMethod;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    // https://dev-pengun.tistory.com/entry/Spring-Boot-CORS-%EC%84%A4%EC%A0%95%ED%95%98%EA%B8%B0
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(
+                        HttpMethod.OPTIONS.name(),
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name()
+                );
+    }
+}
