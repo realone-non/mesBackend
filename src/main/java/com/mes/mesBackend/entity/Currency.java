f053182efed7ee7b204a3b00829804692103e9df
heeeun <gmldms7868@naver.com>
2021-11-23 11:06:17 +0900

    중간

--- src/main/java/com/mes/mesBackend/entity/Currency.java
+++ src/main/java/com/mes/mesBackend/entity/Currency.java
@@ -7,26 +7,34 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
 /*
  * 화폐
  * 화폐 (KRW￦)
  * */
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
 @Entity(name = "CURRENCIES")
 @Data
 public class Currency extends BaseTimeEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = IDENTITY)
     @Column(name = "ID", columnDefinition = "bigint COMMENT '화폐 고유아이디'")
     private Long id;
 
     @Column(name = "CURRENCY", nullable = false, columnDefinition = "varchar(255) COMMENT '화페'")
     private String currency;        // 화폐
 
-    @Column(name = "USE_YN", columnDefinition = "bit COMMENT '사용여부'")
-    private boolean useYn = true;   // 사용여부
-
-    @Column(name = "DELETE_YN", columnDefinition = "bit(1) COMMENT '삭제여부'")
+    @Column(name = "DELETE_YN", columnDefinition = "bit(1) COMMENT '삭제여부'", nullable = false)
     private boolean deleteYn = false;  // 삭제여부
+
+    public void update(Currency newCurrency) {
+        setCurrency(newCurrency.currency);
+    }
+
+    public void delete() {
+        setDeleteYn(true);
+    }
 }
