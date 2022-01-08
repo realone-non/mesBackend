eeb061b2b951932c292fb140e1354c742b7d62db
heeeun <gmldms7868@naver.com>
2022-01-08 17:47:06 +0900

    18-2. 권한등록(사용자 리스트 검색 조회)

--- src/main/java/com/mes/mesBackend/repository/custom/UserRepositoryCustom.java
+++ src/main/java/com/mes/mesBackend/repository/custom/UserRepositoryCustom.java
@@ -1,5 +1,6 @@
 package com.mes.mesBackend.repository.custom;
 
+import com.mes.mesBackend.dto.response.UserAuthorityResponse;
 import com.mes.mesBackend.entity.User;
 
 import java.util.List;
@@ -7,4 +8,9 @@ import java.util.List;
 public interface UserRepositoryCustom {
     // 직원(작업자) 전체 조회 검색조건: 부서, 사번, 이름
     List<User> findAllCondition(Long departmentId, String userCode, String korName);
+
+    // ======================================= 18-2. 권한등록 =======================================
+    // 사용자 리스트 검색 조회
+    // 검색조건: 사용자 ID(userCode), 이름
+    List<UserAuthorityResponse> findUserAuthorityResponsesByCondition(String userCode, String userName);
 }
--- src/main/java/com/mes/mesBackend/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/mes/mesBackend/repository/impl/UserRepositoryImpl.java
@@ -1,8 +1,11 @@
 package com.mes.mesBackend.repository.impl;
 
+import com.mes.mesBackend.dto.response.UserAuthorityResponse;
+import com.mes.mesBackend.entity.QDepartment;
 import com.mes.mesBackend.entity.QUser;
 import com.mes.mesBackend.entity.User;
 import com.mes.mesBackend.repository.custom.UserRepositoryCustom;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +19,7 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
 
     final QUser user = QUser.user;
+    final QDepartment department = QDepartment.department;
 
     // 직원(작업자) 전체 조회 검색조건: 부서, 사번, 이름
     @Override
@@ -27,6 +31,31 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
                         isDepartmentEq(departmentId),
                         isUserCodeContaining(userCode),
                         isKorNameContaining(korName),
+                        isDeleteYnFalse()
+                )
+                .fetch();
+    }
+
+    // ======================================= 18-2. 권한등록 =======================================
+    // 사용자 리스트 검색 조회
+    // 검색조건: 사용자 ID(userCode), 이름
+    @Override
+    public List<UserAuthorityResponse> findUserAuthorityResponsesByCondition(String userCode, String userName) {
+        return jpaQueryFactory
+                .select(
+                        Projections.fields(
+                                UserAuthorityResponse.class,
+                                user.id.as("id"),
+                                user.userCode.as("userCode"),
+                                user.korName.as("korName"),
+                                user.department.deptName.as("deptName")
+                        )
+                )
+                .from(user)
+                .leftJoin(department).on(department.id.eq(user.department.id))
+                .where(
+                        isUserCodeContaining(userCode),
+                        isKorNameContaining(userName),
                         isDeleteYnFalse()
                 )
                 .fetch();
