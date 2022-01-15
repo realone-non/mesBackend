d85e25eea623c2ad90f5a375c5dc70b15e54aa20
heeeun <gmldms7868@naver.com>
2022-01-15 16:06:22 +0900

    8-5. 불량등록 api (아웃소싱 관련 레파지토리 파일 추가)

---
+++ src/main/java/com/mes/mesBackend/repository/OutsourcingReturnRepository.java
@@ -0,0 +1,8 @@
+package com.mes.mesBackend.repository;
+import com.mes.mesBackend.entity.OutsourcingReturn;
+import com.mes.mesBackend.repository.custom.JpaCustomRepository;
+import com.mes.mesBackend.repository.custom.OutsourcingReturnRepositoryCustom;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface OutsourcingReturnRepository extends JpaCustomRepository<OutsourcingReturn, Long>, OutsourcingReturnRepositoryCustom {
+}
