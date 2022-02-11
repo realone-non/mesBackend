6430e43b2abd9da3f2dc0171659d1d216cbacfe3
heeeun <gmldms7868@naver.com>
2022-02-11 16:52:38 +0900

    토큰 미입력시 401 추가, 검사등록 부적합 검사항목 테이블 추가

---
+++ src/main/java/com/mes/mesBackend/entity/enumeration/InspectionItem.java
@@ -0,0 +1,14 @@
+package com.mes.mesBackend.entity.enumeration;
+
+// 검사항목
+public enum InspectionItem {
+    /*
+    * 외관(파손): EXTERIOR_DAMAGE
+    * 외관(포장상태): EXTERIOR_PACKAGING
+    * 외관(인쇄상태): EXTERIOR_PRINTING
+    * 치수: SIZE
+    * 정량: DOSE
+    * [외관(파손): EXTERIOR_DAMAGE, 외관(포장상태): EXTERIOR_PACKAGING, 외관(인쇄상태): EXTERIOR_PRINTING, 치수: SIZE, 정량: DOSE]
+    * */
+    EXTERIOR_DAMAGE, EXTERIOR_PACKAGING, EXTERIOR_PRINTING, SIZE, DOSE
+}
