2149743af5719096807fb5a92d033a607b4baf52
heeeun <gmldms7868@naver.com>
2022-02-14 19:03:02 +0900

    inspectionType NONE 추가

--- src/main/java/com/mes/mesBackend/entity/enumeration/InspectionType.java
+++ src/main/java/com/mes/mesBackend/entity/enumeration/InspectionType.java
@@ -6,7 +6,7 @@ public enum InspectionType {
     검사유형
     Sampling: SAMPLING,
     전수: FULL_INSPECTION
-    [Sampling: SAMPLING, 전수: FULL_INSPECTION]
+    [Sampling: SAMPLING, 전수: FULL_INSPECTION, 없음: NONE]
      */
-    SAMPLING, FULL_INSPECTION
+    SAMPLING, FULL_INSPECTION, NONE
 }
