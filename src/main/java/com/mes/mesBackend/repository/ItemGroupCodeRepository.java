0c3d0fc33adf579b1748cd99c538584de87a1cd8
heeeun <gmldms7868@naver.com>
2022-02-21 17:16:53 +0900

    2022.02.21 (월) 김희은

--- src/main/java/com/mes/mesBackend/repository/ItemGroupCodeRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.mes.mesBackend.repository;
-//
-//import com.mes.mesBackend.entity.ItemGroupCode;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository TODO: 삭제
-//public interface ItemGroupCodeRepository extends JpaRepository<ItemGroupCode, Long> {
-//}
--- src/main/java/com/mes/mesBackend/repository/WorkCenterCodeRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.mes.mesBackend.repository;
-//
-//import com.mes.mesBackend.entity.WorkCenterCode;
-//import org.springframework.data.jpa.repository.JpaRepository;
-// TODO: 삭제
-//public interface WorkCenterCodeRepository extends JpaRepository<WorkCenterCode, Long> {
-//}
