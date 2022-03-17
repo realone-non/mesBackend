83b3178fb6556381bced5b9fb7354c074a685346
heeeun <gmldms7868@naver.com>
2022-03-17 12:14:04 +0900

    2022.03.17 (목) 김희은

--- src/main/java/com/mes/mesBackend/entity/Equipment.java
+++ src/main/java/com/mes/mesBackend/entity/Equipment.java
@@ -123,6 +123,7 @@ public class Equipment extends BaseTimeEntity {
         setWorkProcess(workProcess);
         setLife(newEquipment.life);
         setLastTestDate(newEquipment.lastTestDate);
+        setLotCode(newEquipment.lotCode);
     }
 
     public void delete() {
