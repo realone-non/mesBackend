d6e9dd6f1bfdd0c08166aa498b9fa18eb64a8c9c
heeeun <gmldms7868@naver.com>
2022-03-17 14:09:14 +0900

    2022.03.17 (목) 김희은 업태관련 수정 [테이블 변경사항] - businessType 테이블 삭제 - workPlaceBusinessType 테이블 삭제 - client 테이블 businessType 컬럼 String 으로 타입 변경 - workPlace 테이블 businessType 컬럼 String 으로 타입 변경

--- src/main/java/com/mes/mesBackend/repository/BusinessTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mes.mesBackend.repository;
-
-import com.mes.mesBackend.entity.BusinessType;
-import com.mes.mesBackend.repository.custom.JpaCustomRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BusinessTypeRepository extends JpaCustomRepository<BusinessType, Long> {
-}
