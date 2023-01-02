ca50356fa4e59967934d41554b25c66027a76b4d
sypark <sypark@silverstar-soultion.com>
2023-01-02 16:01:58 +0900

    로그전송 기능 추가 2023-01-02 박수용

--- src/main/java/com/mes/mesBackend/logger/LogSender.java
+++ src/main/java/com/mes/mesBackend/logger/LogSender.java
@@ -24,11 +24,15 @@ public class LogSender {
     public void sendLog(String method, String userIp) throws ParseException {
         RestTemplate restTemplate = new RestTemplate();
         String url = "https://log.smart-factory.kr/apisvc/sendLogData.json";
+        Boolean isTimeOver = true;
         UserLog dbUserLog = userLog.findTop1ByUseMethodOrderByCreatedDateDesc(method);
         String trimMethod = method.trim();
         LocalDateTime nowTime = LocalDateTime.now();
+        if(dbUserLog == null){
+            isTimeOver = dbUserLog.getCreatedDate().plusMinutes(1).isBefore(nowTime);
+        }
 
-        if(dbUserLog != null && trimMethod.equals("GET") && dbUserLog.getCreatedDate().plusMinutes(1).isBefore(nowTime)){
+        if(trimMethod.equals("GET") && isTimeOver){
             // API 전송시 APPLICATION_JSON이 아닌 application/x-www-form-urlencoded로 보내야됨. 객체 형태로 보낼 경우, 해당 폼으로 보내기 안됨.
             // 해당 폼으로 보내야 할 경우는 MultiValueMap을 사용해야 함.
             // https://velog.io/@yeon/RestTemplate-%EC%9C%BC%EB%A1%9C-applicationx-www-form-urlencoded-%ED%83%80%EC%9E%85-%EC%9A%94%EC%B2%AD-%EC%A0%84%EC%86%A1%ED%95%98%EA%B8%B0-ITDA-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8
